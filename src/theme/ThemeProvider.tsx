26b4bbe193b64087d7de8d206017f1ba4ff5f930
Harshit Raj <harshitr20@iitk.ac.in>
2022-12-08 13:08:31 +0530

    Add 404

---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { ThemeProvider } from '@mui/material';
+import { themeCreator } from './base';
+import { StylesProvider } from '@mui/styles';
+
+export const ThemeContext = React.createContext(
+  (themeName: string): void => {}
+);
+
+const ThemeProviderWrapper: React.FC<{ children: React.ReactNode }> = (props) => {
+  const curThemeName = 'PureLightTheme';
+  const [themeName, _setThemeName] = useState(curThemeName);
+  const theme = themeCreator(themeName);
+  const setThemeName = (themeName: string): void => {
+    localStorage.setItem('appTheme', themeName);
+    _setThemeName(themeName);
+  };
+
+  useEffect(() => {
+    const localThemeName = localStorage.getItem('appTheme');
+    if (localThemeName) {
+      setThemeName(localThemeName);
+    }
+  }, []);
+  
+
+  return (
+    <StylesProvider injectFirst>
+      <ThemeContext.Provider value={setThemeName}>
+        <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
+      </ThemeContext.Provider>
+    </StylesProvider>
+  );
+};
+
+export default ThemeProviderWrapper;
