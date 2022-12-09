0e4f2c693cba1f5290afaf2cf9d456021e2d8b72
Harshit Raj <harshitr20@iitk.ac.in>
2022-12-10 02:04:14 +0530

    Add layout with sidebar // WIP

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,34 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { CssBaseline } from "@mui/material"
+import { NextPage } from "next"
+import { FC } from "react"
 
-import ThemeProviderWrapper from "../theme/ThemeProvider"
+import { SidebarProvider } from "src/contexts/SidebarContext"
+import EmptyLayout from "src/layouts/EmptyLayout"
+import ThemeProviderWrapper from "src/theme/ThemeProvider"
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <ThemeProviderWrapper>
-    <CssBaseline />
-    <Component {...pageProps} />
-  </ThemeProviderWrapper>
-)
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  layout?: FC
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const Layout = Component.layout || EmptyLayout
+  return (
+    <SidebarProvider>
+      <ThemeProviderWrapper>
+        <CssBaseline />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProviderWrapper>
+    </SidebarProvider>
+  )
+}
 
 export default App
